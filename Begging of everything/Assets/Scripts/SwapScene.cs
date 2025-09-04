ce1de6e208f82a553c7e14045bf096c4751c1ae7
FelipeMacariMagalhaes <felipe.magalhaes@alunos.sc.senac.br>
2025-09-04 10:27:05 -0300

    ScenePrincipal

---
+++ Begging of everything/Assets/Scripts/SwapScene.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapScene : MonoBehaviour
+{
+
+    
+    private bool jogadorPerto = false;
+
+
+
+    void Update()
+
+    {
+
+        if (jogadorPerto && Input.GetKeyDown(KeyCode.E))
+
+        {
+            SceneManager.LoadScene("FimDoJogo");
+
+        }
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+
+    {
+
+        if (other.CompareTag("Player"))
+
+        {
+
+            jogadorPerto = true;
+
+            
+
+        }
+
+    }
+
+
+
+    private void OnTriggerExit2D(Collider2D other)
+
+    {
+
+        if (other.CompareTag("Player"))
+
+        {
+
+            jogadorPerto = false;
+
+            
+
+        }
+
+    }
+}
